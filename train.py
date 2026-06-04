ffb96c71a37c4463cc665c843e8b7dc72a8570f9
cjq <522114592@qq.com>
2026-06-04 17:10:15 +0800

    修复

--- train.py
+++ train.py
@@ -203,7 +203,7 @@ def main():
         for idx in client_indices
     ]
     fisher_client_loaders = None
-    if cfg["expert_agg_method"] == "fisher_total":
+    if cfg["expert_agg_method"] in {"fisher_total", "fisher_history_wolf"}:
         fisher_train_ds = get_deterministic_train_dataset(
             cfg["dataset"], cfg["data_root"]
         )
